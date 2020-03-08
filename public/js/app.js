eb0fb3609bced7e2777bf2f176f5f2d00ed15ee6
Marcus Holland <eikeland.marcus@gmail.com>
2020-03-08 17:07:09 +0100

    Remove console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,3 @@
-
-
-console.log("Clients side javascrupt file is loaded")
-
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
