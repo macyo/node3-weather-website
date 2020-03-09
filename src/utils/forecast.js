7006dc12a9c9935943c361b0ba2881e2e27612ca
Marcus Holland <eikeland.marcus@gmail.com>
2020-03-09 17:05:46 +0100

    Added high and low

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -5,6 +5,15 @@ const forecast = (lat, long, callback) => {
 
 const url = 'https://api.darksky.net/forecast/9e00eba07833e3b6718b159ce2153ca9/'+lat+','+long+'?units=si'
 
+function Unix_timestamp(t)
+{
+var dt = new Date(t*1000);
+var hr = dt.getHours();
+var m = "0" + dt.getMinutes();
+var s = "0" + dt.getSeconds();
+return hr+ ':' + m.substr(-2) + ':' + s.substr(-2);  
+}
+
 request({ url, json: true}, (error, {body} ) => {
 
     if (error){
@@ -20,7 +29,9 @@ request({ url, json: true}, (error, {body} ) => {
         const temp = body.currently.temperature
         const chance = body.currently.precipProbability
         const place = body.timezone
-        callback(undefined, body.daily.data[0].summary + ' It is currently ' + temp + ' degrees, with a ' + chance + '% chance of rain')
+        
+
+        callback(undefined, body.daily.data[0].summary + ' It is currently ' + temp + ' degrees, with a ' + chance + '% chance of rain. The high today is ' + body.daily.data[0].temperatureHigh + 'C, with a low of ' + body.daily.data[0].temperatureLow+'C.')
         
 
     
